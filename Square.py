59f974da67bf55144bc6341e3e9b49fb9f01d77e
Navaneeth Sudheer <navneet.sudheer@gmail.com>
2019-06-21 14:23:26 +0800

    Changed Sierpinkski_Triangle back into just generating the triangle without user input and aded Square,py which generates the chaos game fractal for a square

--- Square.py
+++ Square.py
@@ -6,23 +6,27 @@ from random import randint
 def midpoint(point1, point2):
     return [(point1[0] + point2[0])/2, (point1[1] + point2[1])/2]
 
-# three points f the triangle chosen at random 
-v1 = [uniform(0,2),uniform(0,2)]
-v2 = [uniform(0,2),uniform(0,2)]
-v3 = [uniform(0,2),uniform(0,2)]
+v1 = [uniform(0,4),uniform(0,4)]
+v2 = [uniform(0,4),uniform(0,4)]
+v3 = [uniform(0,4),uniform(0,4)]
+v4 = [uniform(0,4),uniform(0,4)]
 
-curr_point = v1 # our initial value for the chaos game
-# Plot 5000 points
-for _ in range(5000):
-    # choose a triangle vertex at random
-    val = randint(0,2)
+curr_point = v1
+lastval = 2000
+for i in range(5000):
+    val = randint(0,3)
+    while (val==lastval):
+        val = randint(0,3)
     if val == 0:
         curr_point = midpoint(curr_point, v1)
     if val == 1:
         curr_point = midpoint(curr_point, v2)
     if val == 2:
         curr_point = midpoint(curr_point, v3)
-    # plot the new current point
+    if val ==3:
+        curr_point = midpoint(curr_point, v4)
+
+    lastval = val
     pylab.plot(curr_point[0],curr_point[1],'m.',markersize=2)
     
 
